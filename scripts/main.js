fc06698aaa24ceeccb494e1b24a7c5c81cd3211c
blibotics <blibotics@gmail.com>
2014-01-26 20:47:46 -0500

    Creating an add task function

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,22 @@
 var app = angular.module("toDoApp", []);
 
+//Creating the 'MainCtrl' controller for the 'toDoApp'
 app.controller('MainCtrl', function($scope){
-  
+
+//Region Initialization
+  //Creating an array to hold all the tasks
+  $scope.masterList = [];
+
+
+//Region Functions
+  //addTask Fn adds a new task to the masterList array
+  $scope.addTask = function(obj){
+
+    if(obj === undefined){
+      return false;
+    }
+
+    $scope.masterList.push(obj);
+    $scope.newTask = '';
+  };
 });
